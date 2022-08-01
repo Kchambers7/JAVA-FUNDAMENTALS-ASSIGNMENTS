1f9dc2de0d87ed511d59c112d463819ba1788c24
Katelyn Chambers <kchambersofficial@yahoo.com>
2022-08-01 16:43:30 -0700

    first java program assign

---
+++ first.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        // your code goes here
+
+        System.out.println(" My name is KC");
+        System.out.println(" My age is 25");
+        System.out.println(" My hometown is Darlington, SC");
+
+    }
+}
